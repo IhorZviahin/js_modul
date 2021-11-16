3a42fbd54808600fc5ec0c726bd0c59814904c12
IhorZviahin <igorzvjagin26@gmail.com>
2021-11-16 22:50:38 +0200

    clw_js

---
+++ lesson_js_3/clw_3_js/clw_3_js.js
@@ -0,0 +1,22 @@
+let mass = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+
+// block 1
+// let i = 0;
+// while (i < mass.length) {
+//     console.log(mass[i])
+//     i++
+// }
+
+//block 2
+// for (i = 0; i < mass.length; i++){
+//     console.log(mass[i])
+// }
+
+//block 3
+let i = 0;
+while (i < mass.length){
+    if (i % 2 === 0){
+     console.log(mass[i])
+    }
+        i++
+}
