5fbc1efb6153bc8c45afe93b5598e1b076f5852c
IhorZviahin <igorzvjagin26@gmail.com>
2021-11-16 21:15:42 +0200

    hw3_js

---
+++ lesson_js_3/hw_js_3/hw3.js
@@ -0,0 +1,184 @@
+// block 1
+
+// let a = [ 4,4334,123, 89, 55,
+//
+// "hello", "train", "target" , "left" , "how" ,
+//
+// 44, 54, "js", true , false ];
+// for ( ab = 0 ; ab < a.length; ab++ ){
+//
+//     console.log(a[ab]);
+// }
+
+
+// block 2
+
+//  let massa = [];
+//
+// massa [0] = "one";
+// massa [1] = "lima";
+// massa [2] = "fish";
+// massa [3]  = 'line';
+// massa [4] = 3;
+//
+//  console.log(massa)
+
+
+// block 3
+
+// for ( let ar = 2; ar<12; ar++){
+//     document.write("<div> 21e12 </div>")
+// }
+
+
+// block 4
+
+// for ( let ar = 2; ar<12; ar++){
+//     document.write(`<div> lal ${ar}</div>`)
+// }
+
+// block 5
+
+
+//  let w = 0;
+// while (w<20){
+//     w++
+//     document.write("<h1>dsf</h1>")
+// }
+
+// block 6
+
+
+// let ww = 0;
+// while (ww<20){
+//     ww++
+//     document.write(`<h1>sdfs ${ww} </h1>`)
+// }
+
+
+// block 7
+
+// let masseven = [12, 32, 43, 23, 99, 66, 45, 65, 34, 22];
+//
+// for (let i = 0; i < masseven.length; i++) {
+//
+//     console.log(masseven[i]);
+// }
+
+
+// block 8
+//
+// let maseight = ["ree", "pop", "ted", "run", "feel", "cool", "look", "sed", "let", "del"]
+//
+// for (let i = 0; i < maseight.length; i++) {
+//     console.log(maseight[i])
+// }
+
+//block 9
+//
+// let masnine = [1, 2, 3, 4, 5, "six", "seven", "mas", false, true]
+//
+// for (let i = 0; i < masnine.length; i++) {
+//     console.log(masnine[i])
+// }
+
+
+// block10
+
+// let masten = [1, 2, 3, 4, 5, "six", "seven", 1, "mas", false, true]
+//
+// for (let i = 0; i < masten.length; i++) {
+//
+//
+//      if (typeof masten[i] === 'boolean'){
+//
+//         console.log(masten[i])
+//     }
+//
+//         }
+//
+// console.log("dsferfe")
+//
+// let mastenq = [1, 2, 3, 4, 5, "six", "seven", 1, "mas", false, true]
+//
+// for (let i = 0; i < mastenq.length; i++){
+//     if
+//     (typeof mastenq[i] === 'number')
+//     {
+//         console.log(mastenq[i])
+//     }
+//
+// }
+//
+// console.log("dsferfe")
+//
+//
+// let mastenw = [1, 2, 3, 4, 5, "six", "seven", 1, "mas", false, true]
+//
+// for (let i = 0; i < mastenw.length; i++){
+//     if
+//         (typeof mastenw[i] === 'string')
+//         {
+//             console.log(mastenw[i])
+//         }
+//
+// }
+
+
+//block 11
+
+// let array = [];
+//
+// array [0] = "kek";
+// array [1] = "rrr";
+// array [2] = "sss";
+// array [3] = 1;
+// array [4] = 2;
+// array [5] = 312;
+// array [6] = 334;
+// array [7] = 387;
+// array [8] = false;
+// array [9] = true;
+//
+// for (let i = 0; i < array.length; i++) {
+//     console.log(array[i])
+// }
+
+// block 12
+
+// for ( let i = 1; i < 11; i++){
+//     console.log(`ds ${i}`)
+//     document.write(`<div>sdfs ${i}</div>`)
+// }
+
+/// 12.1
+//
+// for (let i = 1; i<101; i++){
+//
+//     console.log(`cons ${i}`)
+//    document.write(`<div>doc ${i}</div>`)
+// }
+
+// 12.2
+
+// for (let i = 1; i < 100;i+=2){
+//     console.log(`cons2 ${i}`)
+//    document.write(`<div>doc2 ${i}</div>`)
+// }
+
+// 12.3
+
+// for (let i = 1; i < 100; i++) {
+//     if (i % 2 === 0) {
+//
+//         console.log(i)
+//     }
+//
+// }
+
+// for (let i = 1; i < 101; i++) {
+//     if (i % 2 !== 0) {
+//         document.write(`<div>dcd ${i}</div>`)
+//         console.log(`fcf ${i}`)
+//     }
+// }
