cb9d3e0a012938c8ce7b4ade1b9ce7a53cab0fe0
IhorZviahin <igorzvjagin26@gmail.com>
2021-11-17 23:25:11 +0200

    clw_js_3

--- lesson_js_3/clw_3_js/clw_3_js.js
+++ lesson_js_3/clw_3_js/clw_3_js.js
@@ -3,20 +3,115 @@ let mass = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
 // block 1
 // let i = 0;
 // while (i < mass.length) {
-//     console.log(mass[i])
+//     console.log(mass[i]);
 //     i++
 // }
 
 //block 2
 // for (i = 0; i < mass.length; i++){
-//     console.log(mass[i])
+//     console.log(mass[i]);
 // }
 
-//block 3
-let i = 0;
-while (i < mass.length){
-    if (i % 2 === 0){
-     console.log(mass[i])
-    }
-        i++
-}
+//block 3 з непарним індексом
+// let i = 0;
+// while (i < mass.length) {
+//     if (i % 2 !== 0) {
+//         console.log(mass[i]);
+//     }
+//     i++
+// }
+
+//block 4 з непарним індексом
+
+// for (i = 0; i < mass.length; i++) {
+//     if (i % 2 !== 0) {
+//         console.log(mass[i]);
+//     }
+// }
+
+//block 5 тільки парні  значення
+// let i = 0;
+// while (i < mass.length) {
+//     if (i % 2 === 0) {
+//         console.log(mass[i]);
+//     }
+//     i++
+// }
+
+//block 6 з непарним індексом
+
+// for (i = 0; i < mass.length; i++) {
+//     if (i % 2 === 0) {
+//         console.log(mass[i]);
+//     }
+// }
+
+// block 7
+
+// for (let i = 0; i < mass.length; i++) {
+//     if (mass[i] % 3 === 0) {
+//         mass[i] = "okten"
+//     }
+//     console.log(mass[i]);
+// }
+
+// block 8
+
+// let i = mass.length-1;
+// while (i > 0) {
+//         console.log(mass[i]);
+//     i--
+// }
+
+
+// // block 9
+// for (let i = mass.length - 1; i > 0; i--) {
+//     console.log(mass[i]);
+// }
+
+
+//block 10
+//
+// let i = mass.length-1;
+// while (i > 0) {
+//     if (i % 2 !== 0) {
+//         console.log(mass[i]);
+//     }
+//     i--
+// }
+
+// block 11
+
+// for (let i = mass.length - 1; i > 0; i--) {
+//     if (i % 2 !== 0) {
+//         console.log(mass[i]);
+//     }
+// }
+
+//block 12
+//
+// let i = mass.length-1;
+// while (i > 0) {
+//     if (i % 2 === 0) {
+//         console.log(mass[i]);
+//     }
+//     i--
+// }
+
+// block 13
+
+// for (let i = mass.length - 1; i > 0; i--) {
+//     if (i % 2 === 0) {
+//         console.log(mass[i]);
+//     }
+// }
+
+//block 14замінити кожне число кратне 3 на слово "okten"(--)
+
+
+// for (let i = mass.length - 1; i > 0; i--) {
+//     if (mass[i] % 3 === 0) {
+//         mass[i] = "okten"
+//     }
+//     console.log(mass[i]);
+// }
