4ca7158d66c620e5db06b6a4743923abdb79aa0c
IhorZviahin <igorzvjagin26@gmail.com>
2021-11-18 00:45:05 +0200

    clw_js_2

---
+++ lesson_js_2/clw_2_js/clw_2_js.js
@@ -0,0 +1,84 @@
+//block 1
+
+// let anumber = +prompt("введите первое число");
+// let bnumber = +prompt("введите второе число");
+//
+// if (anumber > bnumber) {
+//     alert("первое число больше")
+// } else if (anumber < bnumber) {
+//     alert("второе число больше")
+// } else if (anumber == bnumber) {
+//     alert("числа равны")
+// }
+//block 2
+
+// let numflat = prompt("какой номер квартиры?")
+//
+// if (numflat > 0 && numflat<21){
+//     alert("1 подьезд")
+// }if (numflat > 20 && numflat<49){
+//     alert("2 подьезд")
+// }if (numflat > 48 && numflat<91){
+//     alert("3 подьезд")
+// }
+
+//block 3
+
+// let number = +prompt("введите число 10");
+// if (number === 10) {
+//     alert("вірно")
+// } else if (number !== 10)
+// {
+//     alert("НЕВІРНО")
+// }
+
+//block 4
+
+// let x = 44
+// if (typeof x === "number") {
+//     console.log("1")
+// } else if (typeof x === "string") {
+//     console.log("2")
+// } else if (typeof x === "boolean" && typeof x === "object") {
+//     console.log("3")
+// } else {
+//     console.log('??')
+// }
+
+//block 5
+
+// let temp = +prompt("Яка темпаратура за вікном?");
+// if( temp>9 && temp<23){
+//     alert("ми йдемо ВЧИТИСЯ");
+// } else {
+//     alert("сидимо вдома і вчимося ОНЛАЙН");
+// }
+
+// block 6
+
+// let givaw = +prompt("Вибери свій приз, від 1 до 5!");
+// if (givaw > 0 && givaw < 6) {
+//     switch (givaw) {
+//
+//         case 1:
+//             console.log("тесла модел х");
+//             break;
+//
+//         case 2:
+//             console.log("iphone11");
+//             break;
+//         case 3:
+//             console.log("зошит в клітинку");
+//             break;
+//         case 4:
+//             console.log("похід у кіно");
+//             break;
+//         case 5:
+//             console.log("часи");
+//             break;
+//     }
+// }
+// else
+//     {
+//         console.log("число не вірне!")
+//     }
