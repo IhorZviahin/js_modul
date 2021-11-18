ae55d7aedd8702011bdbebbfc9db140405bcb148
IhorZviahin <igorzvjagin26@gmail.com>
2021-11-18 21:28:21 +0200

    hw_js_4

---
+++ lesson_js_4/hw_js_4/js4.js
@@ -0,0 +1,87 @@
+// block1
+// function s(a, b) {
+//     let results = a * b;
+//     return results;
+// }
+// let results = s(3,5)
+// console.log(results);
+
+// block 2
+
+// function sk(r){
+//     let results = Math.PI*r**2;
+//     return results;
+// }
+// let results =sk(5);
+// console.log(results);
+
+// block 3
+
+// function scc(h, r) {
+//     let results = Math.PI * r ** 2 * h;
+//     return results;
+// }
+// let results = scc(45 , 5);
+// console.log(results);
+
+//block 4
+
+
+// let masone = [12, 32, 43, 23, 99, 66, 45, 65, 34, 22];
+// let mastwo = [54, 23, 12, 43, 9876, 45666, 545, 645, 1, 7654]; //вставлял его что бы глянуть что работает с разными масивами
+//
+// function mas(a) {
+//     for (let i = 0; i < a.length; i++) {
+//         console.log(a[i]);
+//     }
+// }
+// mas(masone)
+
+//block 5
+
+// function par(a) {
+//     document.write(`<p>${a}</p>`)
+//     document.write(`<p>${a}</p>`)
+// }
+// par("ceghjnehblehjeeuidebhjduiodqhbj")
+
+//block 6
+// function li(a){
+//     document.write(`<ul>`)
+//     document.write(`<li>${a}</li>`)
+//     document.write(`<li>${a}</li>`)
+//     document.write(`<li>${a}</li>`)
+//     document.write(`</ul>`)
+// }
+// li(3445)
+
+//block 7
+// function li(a, b) {
+//     document.write(`<ul>`)
+//     for (let i = 0; i < a; i++) {
+//         document.write(`<li>${b}</li>`)
+//     }
+//     document.write(`</ul>`)
+// }
+// li(5, "jkevjn");
+
+//block 8
+// let fist = [2132, 43, "fewaf", "wefw", 32, true];
+// function mas(a) {
+//     document.write(`<ul>`)
+//     for (let i = 0; i < a.length; i++) {
+//         document.write(`<li>${a[i]}</li>`)
+//     }
+//     document.write(`</ul>`)
+// }
+// mas(fist)
+
+// block 9
+
+// let mas = [
+//     {
+//        id: 43;
+//        name: "Leps";
+//        age:54;
+//     }
+// ];
