5eae91375553d93531f3a2cb0c9f66732669755b
IhorZviahin <igorzvjagin26@gmail.com>
2021-11-23 00:36:20 +0200

    clw_js_4

---
+++ lesson_js_4/clw_js_4/clw_js_4.js
@@ -0,0 +1,88 @@
+// block 1
+// function min(a, b, c) {
+//     if (a < b && a < c) {
+//         console.log(a);
+//     } else if (b < a && b < c) {
+//         console.log(b)
+//     } else if (c < a && c < b)
+//     {
+//         console.log(c)
+//     }
+// }
+// min(3,-43223,43)
+//block2
+// function max(a,b,c){
+//     if (a > b && a > c) {
+//         console.log(a);
+//     } else if (b > a && b > c) {
+//         console.log(b)
+//     } else if (c > a && c > b)
+//     {
+//         console.log(c)
+//     }
+// }
+// max(23,-432,55)
+// block 3
+
+// let mas = [1, 12, 23, 765, 3];
+// function searchmaxnumber(aaa) {
+//     let a = mas[0];
+//     for (let i = 0; i < mas.length; i++) {
+//         if (a < aaa[i]) {
+//             a = aaa[i];
+//         }
+//     }
+//     return a
+// }
+// let act = searchmaxnumber(mas)
+// {
+//     console.log(act)
+// }
+
+// block 4
+// let mas = [32423, 12, 23, 765, 3];
+// function searchminnumber(mas) {
+//     let a = mas[0];
+//     for (let i = 0; i < mas.length; i++) {
+//         if (a > mas[i]) {
+//             a = mas[i];
+//         }
+//     }
+//     return a
+// }
+// let act = searchminnumber(mas)
+// {
+//     console.log(act)
+// }
+
+//block 5 -> створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його.
+// let mas = [43, 12, 43, 23];
+//
+// function summa(a) {
+//     let chisla = 0;
+//     for (i = 0; i < mas.length; i++) {
+//         chisla = chisla + a[i]
+//     }
+//     return chisla
+// }
+// let dfef = summa(mas)
+//     console.log(dfef)
+
+//block 6 - створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
+
+// let mas = [43, 12, 43, 23];
+//
+// function summa(a) {
+//     let chisla = 0;
+//     for (i = 0; i < mas.length; i++) {
+//         chisla = (chisla + a[i]);
+//     }
+//     return chisla/ mas.length
+// }
+// let dfef = summa(mas)
+// console.log(dfef)
+
+// block7 створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше
+
+
+
