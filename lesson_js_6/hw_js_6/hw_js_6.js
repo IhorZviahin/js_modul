84500222498fe219a639a391783f43319cf05eef
IhorZviahin <igorzvjagin26@gmail.com>
2021-11-25 21:32:58 +0200

    clw_js_6

---
+++ lesson_js_6/hw_js_6/hw_js_6.js
@@ -0,0 +1,77 @@
+// block 1 -  Знайти та вивести довижину настипних стрінгових значень
+//     'hello world', 'lorem ipsum', 'javascript is cool'
+
+// let str = "hello world";
+// let strtwo = "lorem ipsum";
+// let strthree = "javascript is cool";
+//
+// console.log(str.length);
+// console.log(strtwo.length);
+// console.log(strthree.length);
+
+// block 2 - Перевести до великого регістру наступні стрінгові значення
+//'hello world', 'lorem ipsum', 'javascript is cool'
+
+// let str = "hello world";
+// let strtwo = "lorem ipsum";
+// let strthree = "javascript is cool";
+//
+// let toUpperCase = str.toUpperCase();
+// console.log(toUpperCase);
+// let toUpperCase2 = strtwo.toUpperCase();
+// console.log(toUpperCase2);
+// let toUpperCase3 = strthree.toUpperCase();
+// console.log(toUpperCase3);
+
+// block 3 - - Перевести до нижнього регістру настипні стрінгові значення
+//       'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
+
+// let str = "HELLO WORLD";
+// let strtwo = "LOREM IPSUM";
+// let strthree = "JAVASCRIPT IS COOL";
+//
+// let toLowerCase = str.toLowerCase();
+// console.log(toLowerCase);
+// let toLowerCase2 = strtwo.toLowerCase();
+// console.log(toLowerCase2);
+// let toLowerCase3 = strthree.toLowerCase();
+// console.log(toLowerCase3);
+
+// block 4 - Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
+
+// let str = '     cx       ds   dirty string   ';
+// console.log(str.trim());
+
+// block 5 -  Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
+//     let str = 'Каждый охотник желает знать';
+//     let arr = stringToarray(str);
+//     document.writeln(arr); // ['Каждый', 'охотник', 'желает', 'знать']
+
+
+let str = 'Каждый охотник желает знать';
+console.log(str.split(''));
+
+
+
+
+
+
+
+// block 6- Напишіть функцію delete_characters(str, length), яка повертає підрядок, що складається із зазначеної кількості символів.
+//     let str = 'Каждый охотник желает знать';
+// document.writeln(delete_characters(str, 7)); // Каждый
+//
+// block 7 - Напишіть функцію insert_dash(str), яка приймає рядок str як аргумент і вставляє тире (-) між словами. При цьому всі символи рядка необхідно перевести у верхній регістр.
+//     let str = "HTML JavaScript PHP";
+// document.writeln(insert_dash(str)); // 'HTML-JAVASCRIPT-PHP'
+//
+// block 8 - Напишіть функцію, яка приймає рядок як аргумент і перетворює регістр першого символу рядка з нижнього регістру у верхній.
+//
+//
+//
+// bloc 9 - Напишіть функцію capitalize(str), яка повертає рядок, у якому кожне слово починається з великої літери.
+
+
+
+
+
