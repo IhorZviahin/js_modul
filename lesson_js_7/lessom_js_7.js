4327b77f44bd16974773c42ea19e019d23e754fb
IhorZviahin <igorzvjagin26@gmail.com>
2021-11-28 23:52:13 +0200

    clw_js_6

---
+++ lesson_js_7/lessom_js_7.js
@@ -0,0 +1,39 @@
+// block 1 Створити функцію конструктор для об'єктів User з полями id, name, surname , email, phone
+
+
+// function User(id, name, surname, email, phone){
+//     this.id = id
+//     this.name = "name"
+//     this.surname = "surname"
+//     this.email = 'email'
+//     this.phone = 'phone'
+// }
+// // block 2 створити пустий масив, наповнити його 10 об'єктами new User(....)
+// let mas = [
+//     new User(1,'Gogo', 'Adin', 'asd@asd.com', 380945538893),
+//     new User(2,'Oliver', 'Baer', 'asd@asd.com', 380945538893),
+//     new User(3,'Jack', 'Beegle', 'asd@asd.com', 380945538893),
+//     new User(4,'Harry', 'Borror', 'asd@asd.com', 380945538893),
+//     new User(5,'Jacob', 'Braum', 'asd@asd.com', 380945538893),
+//     new User(6,'Charley', 'Senhour', 'asd@asd.com', 380945538893),
+//     new User(7,'Thomas', 'Hisey', 'asd@asd.com', 380945538893),
+//     new User(8,'George', 'Reser', 'asd@asd.com', 380945538893),
+//     new User(9,'Rosy', 'Shown', 'asd@asd.com', 380945538893),
+//     new User(10,'Oscar', 'Shue', 'asd@asd.com', 380945538893),
+// ];
+// console.log(mas);
+
+
+
+// - Взяти масив з  User[] з попереднього завдання, та відфільтрувати , залишивши тільки об'єкти з парними id (filter)
+
+
+
+// - Взяти масив з  User[] з попереднього завдання, та відсортувати його по id. по зростанню (sort)
+
+
+// - створити класс для об'єктів Client з полями id, name, surname , email, phone, order (поле є масивом зі списком товарів)
+// створити пустий масив, наповнити його 10 об'єктами Client
+
+
+// - Взяти масив (Client [] з попереднього завдання).Відсортувати його по кількості товарів в полі order по зростанню. (sort)
