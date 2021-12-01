7976cce0c8eedb03540bcdc4a58493cbb8b0fa58
IhorZviahin <igorzvjagin26@gmail.com>
2021-12-01 22:24:58 +0200

    hw_js_8

---
+++ lesson_js_8/hw_js_8/hw_js_8.js
@@ -0,0 +1,31 @@
+//Напишіть код,  котрий :
+// -- отримує текст з параграфа з id "content"
+// let one = document.getElementById('content')
+// console.log(one)
+//  -- отримує текст з блоку з id "rules"
+// let two = document.getElementById('rules')
+// console.log(two)
+//    -- замініть текст параграфа з id 'content' на будь-який інший
+// one.innerText = "hyugedwvhbjdufiyhdwjndchyu gwghjnduycen";
+//    -- замініть текст параграфа з id 'rules' на будь-який інший
+// two.innerText = 'dhcjwejbcjhwencjkjcncjwkqcna';
+//-- змініть кожному елементу колір фону на червоний
+// let three = document.body.children
+// for (const child of three){
+//     child.style.background = 'red';
+// }
+//-- змініть кожному елементу колір тексту на синій
+// let four = document.body.children
+// for (const child of four) {
+//     child.style.color = 'blue';
+// }
+//    -- отримати весь список класів елемента з id=rules і вивести їх в console.log
+// let five = document.getElementById('rules');
+// console.log(five.classList);
+
+//    -- поміняти колір тексту у всіх елементів fc_rules на червоний
+
+// let six = document.getElementsByClassName('fc_rules')
+// for (const i of six){
+//     i.style.color = 'red';
+// }
